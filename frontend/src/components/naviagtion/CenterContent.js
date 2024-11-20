74252b52894735c845131d4464a31e50a2811f61
khowellmobile <khowellmobile@gmailc.com>
2024-11-20 13:01:57 -0500

    Added preimilary home page

---
+++ frontend/src/components/naviagtion/CenterContent.js
@@ -0,0 +1,11 @@
+import classes from "./CenterContent.module.css";
+
+function CenterContent() {
+    return (
+        <div className={classes.centercontent}>
+            <p>CenterContent</p>
+        </div>
+    );
+}
+
+export default CenterContent;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import TopNav from "../components/naviagtion/TopNav";
+import CenterContent from "../components/naviagtion/CenterContent";
+import BotNav from "../components/naviagtion/BotNav";
+
+function HomePage() {
+    return (
+        <div>
+            <TopNav />
+            <CenterContent />
+            <BotNav />
+        </div>
+    );
+}
+
+export default HomePage;
